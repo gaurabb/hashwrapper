a82568cd300c8fb0be613f93371b13e9e85b9ad0
Gaurab Bhattacharjee <gaurabb@hotmail.com>
2015-07-10 20:58:15 -0700

    Updated tests to use nose. Some aditions to README.md

--- tests/tests.py
+++ tests/tests.py
@@ -1,20 +1,23 @@
+## Default Python modules
 import unittest
-from hashwrapper import hashwrapper
 import sys
+
+## Required Modules
+from nose.tools import *
+
+## Current project modules
+from hashwrapper.hashwrapper import hashwrapper
 
 class TestHashWrapper(unittest.TestCase):
 	## Test that the generateHash function accept an input string and returns a string value containing the hash
 	def test_can_create_hash(self):
 		##Set up the test
 		objGetHash = hashwrapper.HashWrapper()
-
+		
 		## Check that an hash Hex string is returned
 		resultHash = objGetHash.generateHash("TestString")
-		self.assertEqual(type(resultHash), str)	
+		self.assertEqual(type(resultHash), str)
 
 		## Fail Intentionally for now. So we get some message back when everything else passes
 		self.fail('Finish everything!') 	
 
-if __name__ == '__main__':
-	objTestWrapper = TestHashWrapper()
-	objTestWrapper.test_can_create_hash()
