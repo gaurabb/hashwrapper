68b5d5de57bc01cd3ebc81bb34c3fcc1a87e84b2
Gaurab Bhattacharjee <gaurabb@hotmail.com>
2015-07-13 19:35:37 -0700

    Fixed some unnecessary comments.

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from distutils.core import setup
 setup(
   name = 'hashwrapper',
-  packages = ['hashwrapper'], # this must be the same as the name above
+  packages = ['hashwrapper'], 
   version = '0.51',
   description = 'hashwrapper - wrapper around hashing methods [hashlib]',
   author = 'Gaurab B',
   author_email = 'gaurabb@hotmail.com',
-  url = 'https://github.com/gaurabb/hashwrapper', # use the URL to the github repo
-  download_url = 'https://github.com/gaurabb/hashwrapper/tarball/0.5', # I'll explain this in a second
-  keywords = ['hash', 'salt' 'crypto', 'hashlib'], # arbitrary keywords
+  url = 'https://github.com/gaurabb/hashwrapper', 
+  download_url = 'https://github.com/gaurabb/hashwrapper/tarball/0.5', 
+  keywords = ['hash', 'salt' 'crypto', 'hashlib'], 
   classifiers = [],
 )
